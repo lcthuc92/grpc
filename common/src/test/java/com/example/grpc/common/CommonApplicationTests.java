489cc0d286a77d1e49262321e373b4f87b91e84b
lcthuc92 <lcthuc92@gmail.com>
2021-05-03 11:47:55 +0700

    Init source code

---
+++ common/src/test/java/com/example/grpc/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpc.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
